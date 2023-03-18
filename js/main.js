0c1275f498577f8659baa9ce3790806620128ac0
Nixon Andrickson <nixon.andrickson@gmail.com>
2023-03-18 15:24:53 -0700

    First commit. Added all files to git

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+  const url = `https://api.nasa.gov/planetary/apod?api_key=KJLxnKfHyPOe0abse1Lzhu1WbKf7FKlhTwXcy6Gn&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('h1').innerText = data.date
+        document.querySelector('h2').innerText = data.title
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
